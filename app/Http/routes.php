92486952022bc99a5a1cf26e9d626f76e6eb8429
Bryan Miller <gratiafide@yahoo.com>
2015-12-31 19:34:17 -0600

    fixed header

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,10 +9,7 @@ Route::get('/', function(){
  */
 Route::get('/pretend-api', function(){
     // Provide an API for Vue to access the data from, which is all of the tasks in JSON format.
-    // To return this as JSON, you can't put it in a view, it has to be returned directly like return $tasks
-    // or else it won't work.
-    header('Access-Control-Allow-Origin: *');
-
+    // To return this as JSON, you have to be very careful how you do it.  See the view.
     $tasks = App\Task::latest()->get();
-    return $tasks;
+    return view('create_json', compact('tasks'));
 });
